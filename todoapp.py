22d445f8a985429de98c6f4760f11b8fd857c55a
Dileepa KV <8057331+dileepagit@users.noreply.github.com>
2018-03-28 13:29:51 +0530

    Update todoapp.py

--- todoapp.py
+++ todoapp.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://patrycja:mypassword@localhost/todoapp'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://patrycja:mypassword@localhost/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 from views import *
